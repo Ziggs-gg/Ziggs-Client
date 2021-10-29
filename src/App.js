383f77cbd13f76342084cb08a2645cc703cbfa19
Kim, Hyungjun <gudwnd1243@gmail.com>
2021-10-29 17:11:13 +0900

    SubNavbar 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SubNavbar from './components/SubNavbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <SubNavbar />
     </div>
   );
 }
---
+++ src/components/SubNavbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+
+
+function SubNavbar() {
+  
+  return(
+    <div className="subnavbar">
+      <div className="nav"><img src="../img/vector_17.svg"/>리그보기</div>
+      <button className="close"><img src="../img/vector_36.svg"/></button>
+      <ul className="major">
+        <li><a href=""><img src="../img/LCK_Logo.svg" />LCK</a></li>
+        <li><a href=""><img src="../img/LPL_Logo.svg" />LPL</a></li>
+        <li><a href=""><img src="../img/LEC_Logo.svg" />LEC</a></li>
+        <li><a href=""><img src="../img/LCS_Logo.svg" />LCS</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default SubNavbar;
