26aeb6483213f07576f4e596a6c6b804a912f7ba
Moonhyeseong <moonhs97@gmail.com>
2021-12-01 16:17:50 +0900

    onClick event

--- src/components/navbar_main.js
+++ src/components/navbar_main.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-
+let Navbar = styled.div`
+  width: 520px;
+  display: flex;
+`;
 let Navbar_container = styled.div`
   background-color: #2b2b28;
   width: 260px;
   height: 100vh;
-  text-align: center;
 `;
 let Title = styled.div`
   color: white;
@@ -34,6 +36,7 @@ let Search_bar_container = styled.div`
   height: 29px;
   border-radius: 20px;
   margin-top: 30px;
+  margin-left: 24px;
 `;
 let Search_bar_form = styled.form``;
 let Search_bar = styled.div`
@@ -60,25 +63,25 @@ let Search_bar_input = styled.input`
   }
 `;
 let List = styled.ul`
-  display: flex;
   flex-direction: column;
   align-items: center;
   padding: 0px;
-  position: absolute;
-  width: 102px;
+
+  width: 260px;
   height: 290px;
-  left: 79px;
-  top: 141px;
 `;
 let List_item = styled.li`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: flex-end;
+  width: 100%;
   padding: 0px;
+  margin-top: 40px;
   margin-bottom: 40px;
   :hover {
     color: red;
+    border-left: 4px solid red;
   }
 `;
 let List_item_text = styled.span`
@@ -87,56 +90,76 @@ let List_item_text = styled.span`
   font-family: Noto Sans KR;
   font-weight: 500;
 `;
+
+let Subnavbar = styled.div`
+  width: 260px;
+  height: 100vh;
+  background-color: gray;
+
+  color: white;
+  text-align: center;
+`;
 function NavbarMain() {
-  let [toggleNavbar, settoggleNavbar] = useState(false);
+  let [modalMatch, setmodalMatch] = useState(false);
+  let [modalStats, setmodalStats] = useState(false);
+  let [modalLeague, setmodalLeague] = useState(false);
+  let [modalTeam, setmodalTeam] = useState(false);
+  let [modalPlayer, setmodalPlayer] = useState(false);
 
   return (
-    <Navbar_container>
-      <Title>
-        <Logo src="img/LOGO.svg" />
-        <Title_text>ZIGGS.GG</Title_text>
-      </Title>
-      <Search_bar_container>
-        <Search_bar_form action="">
-          <Search_bar className="search-bar">
-            <img src="img/Searching.svg" alt="" />
-            <Search_bar_input type="text" placeholder="검색어를 입력해주세요." />
-          </Search_bar>
-        </Search_bar_form>
-      </Search_bar_container>
-      <List>
-        <a href="#">
-          <List_item>
-            <img src="img/Schedule.svg" />
-            <List_item_text>경기일정</List_item_text>
-          </List_item>
-        </a>
-        <a href="#">
-          <List_item>
-            <img src="img/Stat_Comparisons.svg" />
-            <List_item_text>통계비교</List_item_text>
-          </List_item>
-        </a>
-        <a href="#">
-          <List_item>
-            <img src="img/Leagues.svg" />
-            <List_item_text>리그보기</List_item_text>
-          </List_item>
-        </a>
-        <a href="#">
-          <List_item>
-            <img src="img/Teams.svg" />
-            <List_item_text>구단보기</List_item_text>
-          </List_item>
-        </a>
-        <a href="#">
-          <List_item>
-            <img src="img/Players.svg" />
-            <List_item_text>선수보기</List_item_text>
-          </List_item>
-        </a>
-      </List>
-    </Navbar_container>
+    <Navbar>
+      <Navbar_container>
+        <Title>
+          <Logo src="img/LOGO.svg" />
+          <Title_text>ZIGGS.GG</Title_text>
+        </Title>
+        <Search_bar_container>
+          <Search_bar_form action="">
+            <Search_bar className="search-bar">
+              <img src="img/Searching.svg" alt="" />
+              <Search_bar_input type="text" placeholder="검색어를 입력해주세요." />
+            </Search_bar>
+          </Search_bar_form>
+        </Search_bar_container>
+        <List>
+          <a href="#">
+            <List_item onClick={() => setmodalMatch(!modalMatch)}>
+              <img src="img/Schedule.svg" />
+              <List_item_text>경기일정</List_item_text>
+            </List_item>
+          </a>
+          <a href="#">
+            <List_item onClick={() => setmodalStats(!modalStats)}>
+              <img src="img/Stat_Comparisons.svg" />
+              <List_item_text>통계비교</List_item_text>
+            </List_item>
+          </a>
+          <a href="#">
+            <List_item onClick={() => setmodalLeague(!modalLeague)}>
+              <img src="img/Leagues.svg" />
+              <List_item_text>리그보기</List_item_text>
+            </List_item>
+          </a>
+          <a href="#">
+            <List_item onClick={() => setmodalTeam(!modalTeam)}>
+              <img src="img/Teams.svg" />
+              <List_item_text>구단보기</List_item_text>
+            </List_item>
+          </a>
+          <a href="#">
+            <List_item onClick={() => setmodalPlayer(!modalPlayer)}>
+              <img src="img/Players.svg" />
+              <List_item_text>선수보기</List_item_text>
+            </List_item>
+          </a>
+        </List>
+      </Navbar_container>
+      {modalMatch == true ? <Subnavbar /> : null}
+      {modalStats == true ? <Subnavbar /> : null}
+      {modalLeague == true ? <Subnavbar /> : null}
+      {modalTeam == true ? <Subnavbar /> : null}
+      {modalPlayer == true ? <Subnavbar /> : null}
+    </Navbar>
   );
 }
 
