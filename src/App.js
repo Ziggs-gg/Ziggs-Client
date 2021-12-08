3cb3fd473a0c15103f3b6a5118a14b01ea21b52c
Kim, Hyungjun <gudwnd1243@gmail.com>
2021-12-08 13:06:25 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import SubNavbar from './components/SubNavbar';
+import ShowPlayer from './components/ShowPlayer';
 
 function App() {
   return (
     <div className="App">
-      <SubNavbar />
+      <ShowPlayer />
     </div>
   );
 }
