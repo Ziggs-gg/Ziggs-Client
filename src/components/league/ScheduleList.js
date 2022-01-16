bf2c96b39c6974bb0b47fa6b0a973bec9d31d3f6
Moonhyeseong <moonhs97@gmail.com>
2022-01-16 17:58:11 +0900

    경기일정 데이터 바인딩

--- src/components/league/ScheduleList.js
+++ src/components/league/ScheduleList.js
@@ -1,9 +1,9 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import styled from 'styled-components';
+import Data from '../../data/matchData.js';
 
-
-function ScheduleList(){
+function ScheduleList(props) {
   let Contain = styled.div`
     display: flex;
     width: 280px;
@@ -26,7 +26,7 @@ function ScheduleList(){
   let BoText = styled.span`
     font-size: 12px;
     font-weight: 350;
-    color: #F3F3F3;
+    color: #f3f3f3;
   `;
 
   let TeamNameAndLogo = styled.div`
@@ -63,29 +63,36 @@ function ScheduleList(){
   let TeamAwayLogo = styled.figure`
     margin: 0;
   `;
+  let team1score = props.match.team1score;
+  let team2score = props.match.team2score;
+  let team1Opacity = 0.5;
+  let team2Opacity = 0.5;
 
-  let awayscore = 2;
-  let homescore = 3;
+  if (team1score < team2score) {
+    team2Opacity = 1;
+  } else {
+    team1Opacity = 1;
+  }
 
-  /*let [opacitiy,setOpacitiy] = useState(1);
-  awayscore < homescore ? setOpacitiy(0.5):setOpacitiy(1);*/
-  return(
+  return (
     <Contain>
       <TimeAndBo>
         <TimeText>17:00</TimeText>
         <BoText>BO3</BoText>
       </TimeAndBo>
-      <TeamNameAndLogo>
-        <TeamHomeLogo >
-          <img src="../img/DRX.svg" alt='' />
+      <TeamNameAndLogo style={{ opacity: team1Opacity }}>
+        <TeamHomeLogo>
+          <img src={`../img/${props.match.team1}.svg`} alt="" />
         </TeamHomeLogo>
         <TeamHomeName>DRX</TeamHomeName>
       </TeamNameAndLogo>
-      <MatchScore>{awayscore} - {homescore}</MatchScore>
-      <TeamNameAndLogo>
+      <MatchScore>
+        {props.match.team1score} - {props.match.team2score}
+      </MatchScore>
+      <TeamNameAndLogo style={{ opacity: team2Opacity }}>
         <TeamAwayName>GEN</TeamAwayName>
         <TeamAwayLogo>
-          <img src='../img/GENG.svg' alt='' />
+          <img src={`../img/${props.match.team2}.svg`} alt="" />
         </TeamAwayLogo>
       </TeamNameAndLogo>
     </Contain>
--- src/data/matchData.js
+++ src/data/matchData.js
@@ -2,8 +2,15 @@ export default [
   {
     team1: 'DRX',
     team2: 'T1',
-    team1core: 3,
-    team2core: 2,
+    team1score: 2,
+    team2score: 1,
+    winner: 'DRX',
+  },
+  {
+    team1: 'DWG',
+    team2: 'GENG',
+    team1score: 1,
+    team2score: 2,
     winner: 'DRX',
   },
 ];
