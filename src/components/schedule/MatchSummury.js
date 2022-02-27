b53df1e953596b958b4f4b31834210a3853a9f9f
Moonhyeseong <moonhs97@gmail.com>
2022-02-27 18:29:19 +0900

    2.27

--- src/components/schedule/MatchSummury.js
+++ src/components/schedule/MatchSummury.js
@@ -62,12 +62,53 @@ function MatchSummury() {
     justify-content: space-between;
     margin-top: 1rem;
     width: 100%;
-    height: 100%;
+    height: auto;
   `;
   let BanPick = styled.div`
     width: 260px;
     height: auto;
     border-right: solid 0.5px #838381;
+    padding-right: 11px;
+  `;
+  let BanPickPhase1Container = styled.div`
+    width: 260px;
+    height: 205px;
+    margin-top: 16px;
+    border-bottom: 1px gray solid;
+  `;
+  let BanPickInfo1 = styled.div`
+    width: 260px;
+    height: 168px;
+    margin-top: 8px;
+  `;
+  let ChampInfoRight = styled.div`
+    height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  `;
+  let ChampInfoleft = styled.div`
+    height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  `;
+
+  let BanPickPhase2Container = styled.div`
+    width: 260px;
+    height: 148px;
+    margin-top: 16px;
+    border-bottom: 1px gray solid;
+  `;
+  let BanPickInfo2 = styled.div`
+    width: 260px;
+    height: 112px;
+    margin-top: 8px;
+  `;
+  let PositionSummury = styled.div`
+    width: 260px;
+    height: 134px;
+    margin-bottom: 16px;
   `;
   let SideContentTitle = styled.span`
     font-size: 16px;
@@ -82,7 +123,34 @@ function MatchSummury() {
   `;
   let GameData = styled.div`
     width: auto;
+    height: auto;
   `;
+  let ObjectSummury = styled.div`
+    width: auto;
+
+    margin-bottom: 16px;
+  `;
+  let StatsDetail = styled.div`
+    width: auto;
+  `;
+  let BanPickText = styled.span`
+    font-size: 14px;
+  `;
+  let ChampIcon = styled.img`
+    width: 28px;
+    height: 28px;
+  `;
+  let ChampIconLeft = styled.img`
+    width: 28px;
+    height: 28px;
+    margin-right: 8px;
+  `;
+  let ChampIconRight = styled.img`
+    width: 28px;
+    height: 28px;
+    margin-left: 8px;
+  `;
+
   return (
     <div className="main-contain">
       <Banner>
@@ -106,10 +174,74 @@ function MatchSummury() {
       <Content>
         <BanPick>
           <SideContentTitle>픽 & 밴</SideContentTitle>
+          <BanPickPhase1Container>
+            <BanPickText>밴 페이즈 1</BanPickText>
+            <BanPickInfo1>
+              <ChampInfoRight>
+                <ChampIconLeft src="../../img/InGame/champion/Ziggs.png" />
+                사미라
+              </ChampInfoRight>
+              <ChampInfoleft>
+                그라가스
+                <ChampIconRight src="../../img/InGame/champion/Ziggs.png" />
+              </ChampInfoleft>
+              <ChampInfoRight>
+                <ChampIconLeft src="../../img/InGame/champion/Ziggs.png" />
+                아칼리
+              </ChampInfoRight>
+              <ChampInfoleft>
+                판테온
+                <ChampIconRight src="../../img/InGame/champion/Ziggs.png" />
+              </ChampInfoleft>
+              <ChampInfoRight>
+                <ChampIconLeft src="../../img/InGame/champion/Ziggs.png" />
+                올라프
+              </ChampInfoRight>
+              <ChampInfoleft>
+                레넥톤
+                <ChampIconRight src="../../img/InGame/champion/Ziggs.png" />
+              </ChampInfoleft>
+            </BanPickInfo1>
+          </BanPickPhase1Container>
+          <BanPickPhase1Container>
+            <BanPickText>픽 페이즈 1</BanPickText>
+          </BanPickPhase1Container>
+          <BanPickPhase2Container>
+            <BanPickText>밴 페이즈 2</BanPickText>
+
+            <ChampInfoleft>
+              레넥톤
+              <ChampIconRight src="../../img/InGame/champion/Ziggs.png" />
+            </ChampInfoleft>
+            <ChampInfoRight>
+              <ChampIconLeft src="../../img/InGame/champion/Ziggs.png" />
+              올라프
+            </ChampInfoRight>
+            <ChampInfoleft>
+              레넥톤
+              <ChampIconRight src="../../img/InGame/champion/Ziggs.png" />
+            </ChampInfoleft>
+            <ChampInfoRight>
+              <ChampIconLeft src="../../img/InGame/champion/Ziggs.png" />
+              올라프
+            </ChampInfoRight>
+          </BanPickPhase2Container>
+
+          <BanPickPhase2Container>
+            <BanPickText>픽 페이즈 2</BanPickText>
+          </BanPickPhase2Container>
         </BanPick>
-        <GameData></GameData>
+        <GameData>
+          <ObjectSummury></ObjectSummury>
+          <StatsDetail></StatsDetail>
+        </GameData>
         <Summury>
           <SideContentTitle>요약</SideContentTitle>
+          <PositionSummury></PositionSummury>
+          <PositionSummury></PositionSummury>
+          <PositionSummury></PositionSummury>
+          <PositionSummury></PositionSummury>
+          <PositionSummury></PositionSummury>
         </Summury>
       </Content>
     </div>
